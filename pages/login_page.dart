a5c5720c1ef762d10074161c8799e9e83035867b
MuneiRas22 <162611763+MuneiRas22@users.noreply.github.com>
2024-09-01 21:39:56 +0200

    Update login_page.dart

--- pages/login_page.dart
+++ pages/login_page.dart
@@ -5,15 +5,14 @@ import 'package:modernlogintute/components/square_tile.dart';
 import 'package:modernlogintute/pages/register_page.dart';
 import 'package:modernlogintute/pages/location_selection_page.dart';
 
-
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
-  // text editing controllers
+  // Text editing controllers
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
-  // sign user in method
+  // Sign-in method
   void signUserIn(BuildContext context) {
     // Navigate to the location selection page after sign-in
     Navigator.push(
@@ -34,17 +33,17 @@ class LoginPage extends StatelessWidget {
               children: [
                 const SizedBox(height: 50),
 
-                // logo
+                // Logo
                 const Icon(
-                  Icons.bus_alert,
+                  Icons.directions_bus,
                   size: 100,
                 ),
 
                 const SizedBox(height: 50),
 
-                // welcome back, you've been missed!
+                // Welcome back message
                 Text(
-                  'Hello, Welcome back!',
+                  'Welcome Back!',
                   style: TextStyle(
                     color: Colors.grey[700],
                     fontSize: 16,
@@ -53,7 +52,7 @@ class LoginPage extends StatelessWidget {
 
                 const SizedBox(height: 25),
 
-                // username textfield
+                // Username text field
                 MyTextField(
                   controller: usernameController,
                   hintText: 'Username',
@@ -62,7 +61,7 @@ class LoginPage extends StatelessWidget {
 
                 const SizedBox(height: 10),
 
-                // password textfield
+                // Password text field
                 MyTextField(
                   controller: passwordController,
                   hintText: 'Password',
@@ -71,7 +70,7 @@ class LoginPage extends StatelessWidget {
 
                 const SizedBox(height: 10),
 
-                // forgot password?
+                // Forgot password link
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: Row(
@@ -87,14 +86,14 @@ class LoginPage extends StatelessWidget {
 
                 const SizedBox(height: 25),
 
-                // sign in button
+                // Sign-in button
                 MyButton(
                   onTap: () => signUserIn(context), // Pass context to signUserIn
                 ),
 
                 const SizedBox(height: 50),
 
-                // or continue with
+                // Or continue with
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: Row(
@@ -124,16 +123,16 @@ class LoginPage extends StatelessWidget {
 
                 const SizedBox(height: 50),
 
-                // google + apple + twitter + facebook sign in buttons
+                // Social sign-in buttons
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: const [
-                    // google button
+                    // Google sign-in button
                     SquareTile(imagePath: 'lib/images/google.png'),
 
                     SizedBox(width: 15),
 
-                    // apple button
+                    // Apple sign-in button
                     SquareTile(imagePath: 'lib/images/apple.png'),
 
                     SizedBox(width: 15),
@@ -142,7 +141,7 @@ class LoginPage extends StatelessWidget {
 
                 const SizedBox(height: 50),
 
-                // not a member? register now
+                // Register link
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
